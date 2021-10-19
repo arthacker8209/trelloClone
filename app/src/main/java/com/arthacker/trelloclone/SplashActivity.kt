074922996876c97a8e6f7d9260dbf5a1999aed70
arthacker8209 <2018csdeepak6816@poornima.edu.in>
2021-10-19 12:04:19 +0530

    Splash Screen

---
+++ app/src/main/java/com/arthacker/trelloclone/SplashActivity.kt
@@ -0,0 +1,25 @@
+package com.arthacker.trelloclone
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.WindowManager
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+        Handler().postDelayed({
+
+        startActivity(Intent(this, MainActivity::class.java))
+        }, 2500)
+
+    }
+}
